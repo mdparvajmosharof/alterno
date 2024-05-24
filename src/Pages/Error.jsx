606fcb70d3a941af7dac2b93023b60f5efd389a6
Md. Parvaj Mosharof <mdparvajmosharof17.com>
2024-05-24 21:41:51 +0600

    error page update

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -7,7 +7,7 @@ const Error = () => {
   }, []);
   return (
     <div>
-      <section className="flex items-center h-full p-16 dark:bg-gray-50 dark:text-gray-800">
+      <section className="flex items-center h-full p-16 card shadow-xl">
         <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
           <div className="max-w-md text-center">
             <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-400">
