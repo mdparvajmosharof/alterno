1a7c8f505b51625e298b75b374532599d81dcbf2
Md. Parvaj Mosharof <mdparvajmosharof17.com>
2024-05-24 20:21:53 +0600

    routs updates

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -6,12 +6,6 @@ import Error from "../Pages/Error";
 import Resister from "../Pages/Resister";
 import PrivateRoutes from "./PrivateRoutes";
 import EstateDetails from "../Component/EstateDetails";
-import InquiryForm from "../Pages/InquiryForm";
-import AddTouristsSpots from "../Pages/AddTouristsSpots";
-import UpdateSpot from "../Pages/UpdateSpot";
-import AllTouristsSpots from "../Pages/AllTouristsSpots";
-import SpotDetails from "../Pages/SpotDetails";
-import CountrySpots from "../Pages/CountrySpots";
 import Queries from "../Pages/Queries";
 import AddQuery from "../Pages/AddQuery";
 import MyQueries from "../Pages/MyQueries";
@@ -57,29 +51,6 @@ const router = createBrowserRouter([
             {
                 path: "/recommendationMe",
                 element: <PrivateRoutes><RecommendationsForMe></RecommendationsForMe></PrivateRoutes>
-            },
-            {
-                path:"/allTouristsSpots",
-                element:<AllTouristsSpots></AllTouristsSpots>
-            },{
-                path: "/country/:countryName",
-                element:<CountrySpots></CountrySpots>
-            },
-            {
-                path:"/spotDetails/:id",
-                element: <PrivateRoutes><SpotDetails></SpotDetails></PrivateRoutes>
-            },
-            {
-                path:"/addTouristsSpots",
-                element:<PrivateRoutes><AddTouristsSpots></AddTouristsSpots></PrivateRoutes>
-            },
-            {
-                path: "/update/:id",
-                element: <PrivateRoutes><UpdateSpot></UpdateSpot></PrivateRoutes>
-            },
-            {
-                path: "/inquiryForm",
-                element: <InquiryForm></InquiryForm>
             },
             {
                 path:"/login",
